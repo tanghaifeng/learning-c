4a3f6ff6cd979b883e3afe1b1d33016051ba0042
timtang <thf2345@163.com>
2022-05-10 22:56:30 +0800

    first

---
+++ practice/chapter2/fisrt.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    num = 99;
+    printf("I am a simle ");
+    printf("computer .\n");
+    printf("My favorite number is %d because it is first .\n", num);
+    return 0;
+}
