c90aa67825cb802629329ba577d285b8ccc97085
timtang <thf2345@163.com>
2022-05-11 21:42:51 +0800

    c

--- practice/chapter2/fisrt.c
+++ practice/chapter2/fisrt.c
@@ -7,5 +7,8 @@ int main()
     printf("I am a simle ");
     printf("computer .\n");
     printf("My favorite number is %d because it is first .\n", num);
+    
+    // 阻塞 等待程序击键
+    getchar();
     return 0;
 }
---
+++ practice/chapter2/tow_func.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// function prototype
+void test();
+
+int main()
+{
+    printf("I will summon thf test function .\n");
+    test();
+    return 0; 
+}
+
+void test()
+{
+    printf("test");
+}
