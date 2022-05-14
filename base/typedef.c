6609812235308210d52e3d31ca3418f61acb08b7
timtang <thf2345@163.com>
2022-05-14 18:01:49 +0800

    c

---
+++ base/typedef.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void promotion(int);
+void demotion(int);
+
+int main()
+{
+    // typedef 建立类型别名
+    typedef int a;
+    a b = 0;
+    printf("%zd .\n", sizeof(a));
+
+    // 自动类型转换, 类型升级通常不会有问题，但是降级 较低类型可能会放不下整个数字
+    promotion('t');
+    short c = 2;
+    promotion(c);
+
+    long d = 2;
+    demotion(d);
+    return 0;
+}
+
+void promotion(int size)
+{
+    printf("%zd .\n", sizeof(size));
+
+}
+
+void demotion(int size)
+{
+    printf("%zd .\n", sizeof(size));
+
+}
