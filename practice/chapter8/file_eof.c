ac8a6b6095d7aac056377051deb69b4b2cf0fffb
timtang <thf2345@163.com>
2022-05-15 17:08:52 +0800

    file_eof

---
+++ practice/chapter8/file_eof.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ch;
+    FILE *fp;
+    char fname[50];
+
+    printf("Enter the name of the file: ");
+    scanf("%s", fname);
+    fp = fopen(fname, "r");
+    if (fp == NULL)
+    {
+        printf("Failed to open file. Bye .\n");
+        exit(1);
+    }
+    // getc 从打开的文件中读取一个字符
+    while ((ch = getc(fp)) != EOF)
+    {
+       putchar(ch);
+    }
+    fclose(fp); // 关闭文件
+    
+
+    return 0;
+}
